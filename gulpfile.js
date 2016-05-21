f7683a169a677b9856c23a206ee87eaa896b2afa
ktygch <Ryuhei_ktygch@R-Ktygch-iMac.local>
2016-05-22 01:27:37 +0900

    gulp install

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+// JavaScript Document
+var gulp = require("gulp");
+//var ejs = require("gulp-ejs");
+var sass = require("gulp-sass");
+var uglify = require("gulp-uglify");
+var plumber = require("gulp-plumber");
+
+gulp.task("ejs", function(){
+    gulp.src(["ejs/**/*.ejs", "!ejs/**/_*.ejs"])
+    .pipe(plumber())
+    .pipe(ejs({}, {ext: '.html'}))
+    .pipe(gulp.dest("./html"));
+});
+
+gulp.task("js", function(){
+    gulp.src(["js/**/*.js", "!js/min/**/*.js"])
+    .pipe(plumber())
+    .pipe(uglify())
+    .pipe(gulp.dest("./js/min"));
+});
+
+gulp.task("sass", function(){
+    gulp.src("sass/**/*.scss")
+    .pipe(plumber())
+    .pipe(sass())
+    .pipe(gulp.dest("./css"));
+});
+
+gulp.task("default", function(){
+    gulp.watch(["js/**/*.js", "!js/min/**/*.js"],["js"]);
+    gulp.watch("sass/**/*.scss", ["sass"]);
+    //gulp.watch(["ejs/**/*.ejs", "!ejs/**/_*.ejs"],["ejs"]);
+});
